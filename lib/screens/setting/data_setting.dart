034bb27a5ac9ce2457113a74c1b07a9a8b4fab47
Dogkaebi5 <st9dog@gmail.com>
2023-07-19 18:27:53 +0900

    data setting design

--- lib/screens/setting/data_setting.dart
+++ lib/screens/setting/data_setting.dart
@@ -11,11 +11,12 @@ class DataPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromARGB(255, 240, 240, 240),
       appBar: AppBar(
-        title: Text('데이터 설정'),
-        centerTitle: true,
+        title: Text('데이터 설정',
+          style: TextStyle(fontWeight: FontWeight.bold),),
         leading: IconButton(
-          icon: Icon(Icons.arrow_back_ios_new),
+          icon: Icon(Icons.arrow_back_ios_new,),
           onPressed: (){
             Navigator.push(
               context,
@@ -24,99 +25,160 @@ class DataPage extends StatelessWidget {
           },
         ),  
       ),
-      body: Container(
-        margin: EdgeInsets.only(top: 20, left:20, right:20,),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            InkWell(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const BasicInfo()),
-                );
-              },
-              child: Container(
-                padding: EdgeInsets.all(20),
-                color: Color.fromARGB(255, 245, 226, 255) ,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: const [
-                    Text("기본정보 설정"),
-                    Icon(Icons.arrow_forward_ios),
-                  ]
-                ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(height: 20,),
+          InkWell(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const BasicInfo()),
+              );
+            },
+            child: Container(
+              margin: EdgeInsets.symmetric(horizontal: 12),
+              padding: EdgeInsets.symmetric(vertical: 48, horizontal: 28),
+              decoration: BoxDecoration(
+                color: Colors.deepPurple,
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: [BoxShadow(
+                    color: Colors.grey.withOpacity(.6),
+                    blurRadius: 6,
+                    spreadRadius : 1,
+                    offset: Offset(2, 4)
+                  )],
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("기본정보 설정",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),),
+                  Icon(Icons.arrow_forward_ios,
+                    size: 16,
+                    color: Colors.deepPurple.shade50,
+                  ),
+                ]
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.only(top: 10, bottom: 10),
-              child: Divider(color: Colors.black, thickness: 1,),
-            ),
-            InkWell(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const Interest()),
-                );
-              },
-              child: Container(
-                margin: EdgeInsets.only(bottom: 20),
-                padding: EdgeInsets.all(20),
-                color: Color.fromARGB(255, 245, 226, 255) ,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: const [
-                    Text("관심사 설정"),
-                    Icon(Icons.arrow_forward_ios),
-                  ]
-                ),
+          ),
+          SizedBox(height: 16,),
+          InkWell(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const Interest()),
+              );
+            },
+            child: Container(
+              margin: EdgeInsets.symmetric(horizontal: 12),
+              padding: EdgeInsets.symmetric(vertical: 48, horizontal: 28),
+              decoration: BoxDecoration(
+                color: Colors.deepPurple.shade300,
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: [BoxShadow(
+                    color: Colors.grey.withOpacity(.6),
+                    blurRadius: 6,
+                    spreadRadius : 1,
+                    offset: Offset(2, 4)
+                  )],
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("관심사 설정",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),),
+                  Icon(Icons.arrow_forward_ios,
+                    size: 16,
+                    color: Colors.deepPurple.shade50,
+                  ),
+                ]
               ),
             ),
-            InkWell(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const AddInfo()),
-                );
-              },
-              child: Container(
-                padding: EdgeInsets.all(20),
-                color: Color.fromARGB(255, 245, 226, 255) ,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: const [
-                    Text("추가정보 설정"),
-                    Icon(Icons.arrow_forward_ios),
-                  ]
-                ),
+          ),
+          SizedBox(height: 8,),
+          InkWell(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const AddInfo()),
+              );
+            },
+            child: Container(
+              margin: EdgeInsets.symmetric(horizontal: 12),
+              padding: EdgeInsets.symmetric(vertical: 48, horizontal: 28),
+              decoration: BoxDecoration(
+                color: Colors.purple.shade200,
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: [BoxShadow(
+                    color: Colors.grey.withOpacity(.6),
+                    blurRadius: 6,
+                    spreadRadius : 1,
+                    offset: Offset(2, 4)
+                  )],
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("추가정보 설정",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),),
+                  Icon(Icons.arrow_forward_ios,
+                    size: 16,
+                    color: Colors.deepPurple.shade50,
+                  ),
+                ]
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.only(top: 10, bottom: 10),
-              child: Divider(color: Colors.black, thickness: 1,),
-            ),
-            InkWell(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const Infopermission()),
-                );
-              },
-              child: Container(
-                margin: EdgeInsets.only(bottom: 20),
-                padding: EdgeInsets.all(20),
-                color: Color.fromARGB(255, 245, 226, 255) ,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: const [
-                    Text("데이터 판매범위 설정"),
-                    Icon(Icons.arrow_forward_ios),
-                  ]
-                ),
+          ),
+          SizedBox(height: 20,),
+          InkWell(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const Infopermission()),
+              );
+            },
+            child: Container(
+              margin: EdgeInsets.symmetric(horizontal: 12),
+              padding: EdgeInsets.symmetric(vertical: 32, horizontal: 28),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: [BoxShadow(
+                    color: Colors.grey.withOpacity(.6),
+                    blurRadius: 6,
+                    spreadRadius : 1,
+                    offset: Offset(2, 4)
+                  )],
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [ 
+                  Text("데이터 판매범위 설정",
+                    style: TextStyle(
+                      fontSize: 16,
+                    ),),
+                  Icon(Icons.arrow_forward_ios,
+                    size: 16,
+                    color: Colors.grey,
+                  ),
+                ]
               ),
             ),
-          ]
-        ),
+          ),
+        ]
       ),
     );
   }

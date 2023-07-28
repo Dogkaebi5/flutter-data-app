fe43e041aa5323a254418ddd7bb0f2eca8f835d6
Dogkaebi5 <st9dog@gmail.com>
2023-07-28 17:58:00 +0900

    start. background image, +animation

--- lib/screens/start/app_start.dart
+++ lib/screens/start/app_start.dart
@@ -10,68 +10,79 @@ class AppStartScreen extends StatefulWidget {
 }
 
 class _AppStartScreenState extends State<AppStartScreen> {
+  bool firstAnimatedState = true;
+  bool repeatAnimatedState = true;
+  setFirstAnimatedState(){
+    setState(()=> firstAnimatedState = false);
+    return SizedBox.shrink();
+  }  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body : SafeArea(
-        child: Container(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('assets/data_center.png'),
-              fit: BoxFit.cover,
-            ),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.only(right: 20, left: 20),
-            child: Column(
-              children : [
-                Spacer(),
-                Container(
-                  width: double.infinity,
-                  height: 180,
-                  padding: EdgeInsets.all(20),
-                  margin: EdgeInsets.only(bottom: 40),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    color: Colors.white.withOpacity(.8),
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Spacer(),
-                      SizedBox(
-                        width: double.infinity, 
-                        height: 44, 
-                        child: ElevatedButton(
-                          onPressed:(){
-                            Navigator.push(
-                              context, 
-                              MaterialPageRoute(builder: (context) => const FirebaseAuthPage()),
-                            );
-                          }, 
-                          child: Text("본인인증"), 
-                        )
-                      ),
-                      
-                      TextButton(
-                        onPressed: (){
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child: Stack(
+            children: [
+              AnimatedPositioned(
+                height: MediaQuery.of(context).size.height,
+                duration: Duration(seconds: 14),
+                right: (firstAnimatedState) ? -1000 : (repeatAnimatedState) ? 0 : -999,
+                curve: Curves.linear,
+                onEnd: () => setState(() {
+                  repeatAnimatedState = !repeatAnimatedState;
+                }),
+                child: Image.asset('assets/data_center.png',fit:BoxFit.fitHeight,),
+              ),
+              Container(
+                width: double.infinity,
+                height: 180,
+                padding: EdgeInsets.all(20),
+                margin: EdgeInsets.only(top: MediaQuery.of(context).size.height/1.5, left: 20, right: 20),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.white.withOpacity(.8),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Spacer(),
+                    SizedBox(
+                      width: double.infinity, 
+                      height: 44, 
+                      child: ElevatedButton(
+                        onPressed:(){
                           Navigator.push(
                             context, 
-                            MaterialPageRoute(builder: (context) => const Webview()),
+                            MaterialPageRoute(builder: (context) => const FirebaseAuthPage()),
                           );
                         }, 
-                        child: Text('고객센터', 
-                        style: TextStyle(
-                          decoration: TextDecoration.underline),
-                        )
-                      ),
-                    ]),
+                        child: Text("본인인증"), 
+                      )
+                    ),
+                    TextButton(
+                      onPressed: (){
+                        Navigator.push(
+                          context, 
+                          MaterialPageRoute(builder: (context) => const Webview()),
+                        );
+                      }, 
+                      child: Text('고객센터', 
+                      style: TextStyle(
+                        decoration: TextDecoration.underline),
+                      )
+                    ),
+                    setFirstAnimatedState()
+                  ]
                 ),
-              ]
-            ),
+              ),
+            ]
           ),
         ),
-      ),
+      )
     );
   }
+
 }

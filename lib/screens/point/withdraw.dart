726c3598d9e302f0c2e74d94311582c1ab41b16e
Dogkaebi5 <st9dog@gmail.com>
2023-08-03 13:54:40 +0900

    withdraw: refactoring

--- lib/screens/point/withdraw.dart
+++ lib/screens/point/withdraw.dart
@@ -25,6 +25,11 @@ class _WithdrawScreenState extends State<WithdrawScreen> {
   String? bank;
   String? account;
 
+  withdraw(){
+    context.read<SettingProvider>().pointTest([-inputPoint, fee, tax, amount]);
+    Navigator.pop(context);
+  }
+
   @override
   void initState() {
     super.initState();
@@ -48,125 +53,115 @@ class _WithdrawScreenState extends State<WithdrawScreen> {
         title: Text("출금신청", style: TextStyle(color: Colors.black87,),),
         centerTitle: true,
       ), 
-      body: Column(
-        children: [
-          SizedBox(height: 30,),
-          InkWell(
-            onTap: () async{
-              final List? data = await navPush(context, BankDataScreen());
-              if (data != null) {
-                setState((){
-                  isHasAcc = data[0];
-                  bank = data[1];
-                  account = data[2];
-                });
-              }
-            },
-            child: 
-            Container(
-              width: 280,
-              height: 60,
-              padding: EdgeInsets.symmetric(horizontal: 20),
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.grey.shade300),
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(30),
-                boxShadow: [greyShadow]
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  (isHasAcc)?
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text("출금계좌 : "),
-                        Text("$bank  $account",
-                          style: TextStyle(
-                            color: Colors.deepPurpleAccent,
-                            fontWeight: FontWeight.bold,
-                        )),
-                      ],
-                    )
-                    : Text("출금계좌 등록하기"),
-                  Icon(Icons.arrow_forward_ios,
-                    size: 16,
-                    color: Colors.grey,
-                  ),
-                ]
-              ),
-            ),
-          ),
-          SizedBox(height: 40),
-          TextField(
-            textAlign: TextAlign.center,
-            keyboardType: TextInputType.number,
-            showCursor: false,
-            style: TextStyle(fontSize: 40,),
-            decoration: InputDecoration(
-              counterText: "",
-              border: InputBorder.none,
-              hintText: "0",
-              hintStyle: TextStyle(fontSize: 40, color: Colors.black),
-            ),
-            maxLength: point.toString().length + 1,
-            inputFormatters: [
-              FilteringTextInputFormatter.allow(RegExp('[0-9]')),
-              FilteringTextInputFormatter.deny(RegExp(r'^0+'),),],
-            scribbleEnabled : false,
-            controller: _controller,
-            onTap: () => _controller.selection = TextSelection.collapsed(offset: _controller.text.length),
-            onChanged: (value) {
-              if (value == "") {
-                _controller.clear();
-              }else if (int.parse(value) > point) {
-                setState((){
-                  _controller.setText(point.toString());
-                  inputPoint = int.parse(point.toString());
-                });
-              }else {
-                setState(() => inputPoint = int.parse(value));
-              }
-              setState(() {
-                (inputPoint < 10000) ? fee = 1000 : fee = 0;
-                tax = (inputPoint * 0.033).floor();
-                amount = inputPoint - fee - tax;
-              });
-            },
-          ),
-          Text("포인트를 입력하세요", style: fontSmallGrey),
-          SizedBox(height: 4,),
-          Text('보유포인트 : ${point.toString()} P',),
-          SizedBox(height: 30,),
-          Padding(
-            padding: const EdgeInsets.all(24),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 28),
+        child: Column(
+          children: [
+            SizedBox(height: 30),
+            InkWell(
+              onTap: () async{
+                final List? data = await navPush(context, BankDataScreen());
+                if (data != null) {
+                  setState((){
+                    isHasAcc = data[0];
+                    bank = data[1];
+                    account = data[2];
+                  });
+                }
+              },
+              child:Container(
+                width: 280,
+                height: 60,
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.grey.shade300),
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(30),
+                  boxShadow: [greyShadow]),
+                child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                  Text('출금 수수료'),
-                  Text('${fee.toString()} P'),
-                  ],
+                    (isHasAcc)?
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("출금계좌 : "),
+                          Text("$bank  $account",
+                            style: TextStyle(
+                              color: Colors.deepPurpleAccent,
+                              fontWeight: FontWeight.bold,
+                          )),
+                        ],
+                      )
+                      : Text("출금계좌 등록하기"),
+                    Icon(Icons.arrow_forward_ios,
+                      size: 16,
+                      color: Colors.grey,
+                    ),
+                  ]
+                ),
+              ),
+            ),
+            SizedBox(height: 40),
+            TextField(
+              textAlign: TextAlign.center,
+              keyboardType: TextInputType.number,
+              showCursor: false,
+              style: TextStyle(fontSize: 40,),
+              decoration: InputDecoration(
+                counterText: "",
+                border: InputBorder.none,
+                hintText: "0",
+                hintStyle: TextStyle(fontSize: 40, color: Colors.black),
+              ),
+              maxLength: point.toString().length + 1,
+              inputFormatters: [
+                FilteringTextInputFormatter.allow(RegExp('[0-9]')),
+                FilteringTextInputFormatter.deny(RegExp(r'^0+'),),],
+              scribbleEnabled : false,
+              controller: _controller,
+              onTap: () => _controller.selection = TextSelection.collapsed(offset: _controller.text.length),
+              onChanged: (value) {
+                if (value == "") {
+                  _controller.clear();
+                }else if (int.parse(value) > point) {
+                  setState((){
+                    _controller.setText(point.toString());
+                    inputPoint = int.parse(point.toString());
+                  });
+                }else {
+                  setState(() => inputPoint = int.parse(value));
+                }
+                setState(() {
+                  (inputPoint < 10000) ? fee = 1000 : fee = 0;
+                  tax = (inputPoint * 0.033).floor();
+                  amount = inputPoint - fee - tax;
+                });
+              },
+            ),
+            Text("포인트를 입력하세요", style: fontSmallGrey),
+            SizedBox(height: 4),
+            Text('보유포인트 : ${point.toString()} P',),
+            SizedBox(height: 48),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [Text('출금 수수료'), Text('${fee.toString()} P')],
                 ),
                 Text("10,000P 이상 신청 시 무료",style: fontSmallGrey),
                 SizedBox(height: 4,),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                  Text('사업소득세(3.3%)'),
-                  Text('${tax.toString()} P'),
-                  ],
+                Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [Text('사업소득세(3.3%)'),Text('${tax.toString()} P')],
                 ),
                 Divider(color: Colors.black, thickness: 1,),
-                SizedBox(height: 5,),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                SizedBox(height: 4),
+                Row(mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                   Text('최종 이체 금액',style: fontSmallTitle),
-                  Text((amount > 0) ? '${amount.toString()} 원' : "0 원", 
+                  Text(
+                    (amount > 0) ? '${amount.toString()} 원' : "0 원", 
                     style: fontSmallTitle),
                   ],
                 ),
@@ -174,26 +169,16 @@ class _WithdrawScreenState extends State<WithdrawScreen> {
                 Divider(color: Colors.black, thickness: 1,),
               ],
             ),
-          ),
-          Spacer(),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: ElevatedButton(
+            Spacer(),
+            ElevatedButton(
               style: btnStyle,
-              onPressed: 
-                (amount > 0 && isHasAcc)
-                ? () => passwordDialog(context, withdraw)
-                : null, 
+              onPressed: (amount > 0 && isHasAcc) ? () => passwordDialog(context, withdraw) : null, 
               child: Text('출금신청'),
             ),
-          ),
-          SizedBox(height:40),
-        ]
+            SizedBox(height:40),
+          ]
+        ),
       ),
     );
-  }
-  withdraw(){
-    context.read<SettingProvider>().pointTest([-inputPoint, fee, tax, amount]);
-    Navigator.pop(context);
   }
 }
